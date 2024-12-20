2e6ea02a85d5ee1eef253d890513955c9b323a35
Vadim Dalecky <streamich@gmail.com>
2024-12-20 10:52:59 +0100

    perf: ⚡️ improve block spoiler parsing performance

--- src/markdown/block/parsers.ts
+++ src/markdown/block/parsers.ts
@@ -66,20 +66,23 @@ const heading: TTokenizer<type.IHeading, MdBlockParser<type.TBlockToken>> = (par
 };
 
 const REG_BLOCKQUOTE = /^( *>[^\n]+(\n(?!^ *\[([^\]]+)\]: *<?([^\s>]+)>?(?: +["(]([^\n]+)[")])? *(?:\n+|$))[^\n]+)*)+/;
-const REG_BLOCK_SPOILER = /^ *! ?/gm;
+const REG_BLOCKQUOTE_OUTDENT = /^ *> ?/gm;
+const REG_BLOCK_SPOILER_CHECK = /^ *!/;
+const REG_BLOCK_SPOILER_OUTDENT = /^ *! ?/gm;
 const blockquote: TTokenizer<type.IBlockquote, MdBlockParser<type.TBlockToken>> = (parser, src) => {
   const matches = src.match(REG_BLOCKQUOTE);
   if (!matches) return;
   const subvalue = matches[0];
-  let content = rep(/^ *> ?/gm, '', subvalue);
-  let lineNumber = 1;
-  let nlIndex = -1;
-  while ((nlIndex = content.indexOf('\n', nlIndex + 1)) > -1) lineNumber++;
-  const spoilerLineCount = (content.match(REG_BLOCK_SPOILER) || []).length;
+  let content = rep(REG_BLOCKQUOTE_OUTDENT, '', subvalue);
   let spoiler = false;
-  if (spoilerLineCount === lineNumber) {
-    spoiler = true;
-    content = content.replace(REG_BLOCK_SPOILER, '');
+  const isPotentiallySpoiler = content.match(REG_BLOCK_SPOILER_CHECK);
+  if (isPotentiallySpoiler) {
+    let lineNumber = 1;
+    let nlIndex = -1;
+    while ((nlIndex = content.indexOf('\n', nlIndex + 1)) > -1) lineNumber++;
+    const spoilerLineCount = (content.match(REG_BLOCK_SPOILER_OUTDENT) || []).length;
+    spoiler = spoilerLineCount === lineNumber;
+    if (spoiler) content = content.replace(REG_BLOCK_SPOILER_OUTDENT, '');
   }
   const children = parser.parse(content);
   const node = token<type.IBlockquote>(subvalue, 'blockquote', children);
